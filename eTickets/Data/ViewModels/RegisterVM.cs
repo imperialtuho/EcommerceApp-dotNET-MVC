663b45070504ec63b665a78399cc6a50b2b0d3da
ImperialTuHo <ImperialTuHo@IMPERIALTUHO-PC>
2022-10-10 22:22:54 +0700

    Add project files.

---
+++ eTickets/Data/ViewModels/RegisterVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace eTickets.Data.ViewModels
+{
+    public class RegisterVM
+    {
+        [Display(Name = "Full name")]
+        [Required(ErrorMessage = "Full name is required")]
+        public string FullName { get; set; }
+
+        [Display(Name = "Email address")]
+        [Required(ErrorMessage = "Email address is required")]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        [StringLength(255,ErrorMessage ="Password is at least 8 characters", MinimumLength = 8)]
+        [RegularExpression(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,15}$",ErrorMessage ="Password must contains at least Digit, Lowercase and Uppercase ")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Display(Name = "Confirm password")]
+        [Required(ErrorMessage = "Confirm password is required")]
+        [DataType(DataType.Password)]
+        [Compare("Password", ErrorMessage = "Passwords do not match")]
+        public string ConfirmPassword { get; set; }
+    }
+}
